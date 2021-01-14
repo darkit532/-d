403ea021d750853971bd72eeeae6d6cc23dfdb8e
darkit532 <74464982+darkit532@users.noreply.github.com>
2021-01-14 13:28:42 +0200

    Update точно_не_вирус.sh

--- точно_не_вирус.sh
+++ точно_не_вирус.sh
@@ -1,2 +1,3 @@
-defps1="PS1='\\\[\\\e[39m\\\]\\\\$ \\\[\\\e[31m\\\]darkit\\\[\\\e[0m\\\]\\\[\\\e[39m\\\]\\\u\\\[\\\e[0m\\\]\\\[\\\e[0                       
+defps1="PS1='\\\[\\\e[39m\\\]\\\[\\\e[31m\\\]darkit\\\[\\\e[0m\\\]\\\\$\\\[\\\e[0m\\\]\\\[\\\e[39m\\\]'"
 sed -i "s/.*PS1=.*/$defps1/" /data/data/com.termux/files/usr/etc/bash.bashrc
+
